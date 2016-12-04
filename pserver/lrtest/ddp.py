8b19ed7b3fb9509616b58d0c9cef11ac0344c528
Aleks Kamko <aykamko@gmail.com>
2016-12-03 22:04:46 -0800

    fix ddpg

--- pserver/lrtest/ddp.py
+++ pserver/lrtest/ddp.py
@@ -27,10 +27,14 @@ def wprint(thing, level=3):
         print(thing)
 
 # Start ray
-NUM_WORKERS = 6
-NUM_GLOBAL_ITERS = 100
+NUM_WORKERS = 1 # 6
+MAJOR_ITERS = 10
+MINOR_ITERS = 10
+NUM_GLOBAL_ITERS = MAJOR_ITERS * MINOR_ITERS
+
 NUM_W_SHARDS = 5
-NUM_WORKER_ITERS = NUM_GLOBAL_ITERS * NUM_W_SHARDS
+NUM_WORKER_ITERS = MINOR_ITERS * NUM_W_SHARDS
+
 addr_info = ray.init(start_ray_local=True, num_workers=NUM_WORKERS)
 
 
@@ -39,9 +43,15 @@ ALPHA = 0.1
 BETA = 1
 ETA = 0.1
 
-LAMBDA1 = 4
-LAMBDA2 = 1e-4
+LAMBDA1 = 1e-2 # rub the crystal ball
+LAMBDA2 = 0 # no L2 loss
 
+
+def logistic_loss(w, X, y, alpha):
+    w, c, yz = _intercept_dot(w, X, y)
+
+    # Logistic loss is the negative of the log of the logistic function.
+    return -np.sum(log_logistic(yz)) + .5 * alpha * np.dot(w, w)
 
 # Source: https://github.com/scikit-learn/scikit-learn/blob/a5ab948/sklearn/linear_model/logistic.py#L78
 def logistic_grad(w, X, y, alpha):
@@ -49,9 +59,6 @@ def logistic_grad(w, X, y, alpha):
     grad = np.empty_like(w)
 
     w, c, yz = _intercept_dot(w, X, y)
-
-    # Logistic loss is the negative of the log of the logistic function.
-    loss = -np.sum(log_logistic(yz)) + .5 * alpha * np.dot(w, w)
 
     z = expit(yz)
     z0 = (z - 1) * y
@@ -62,7 +69,7 @@ def logistic_grad(w, X, y, alpha):
     if grad.shape[0] > n_features:
         grad[-1] = z0.sum()
 
-    return loss, grad
+    return grad
 
 def worker_client_initializer():
     return PlasmaClient(addr_info['object_store_name'])
@@ -99,7 +106,7 @@ def async_compute_lr_grads(worker_idx, X_local, y_local, Wid, tauid, X_shard_idx
         W_shard = local_client.pull(Wid, W_range) # TODO: reuse a numpy array here
         X_slice = X_local[:, range(*W_range)]
 
-        _, grad = logistic_grad(W_shard, X_slice, y_local, ALPHA)
+        grad = logistic_grad(W_shard, X_slice, y_local, ALPHA)
         wprint("worker {}: computed grad {} for shard {}".format(worker_idx, i, W_shard_idx))
         yield (W_shard_idx, grad)
 
@@ -120,8 +127,7 @@ def async_compute_lr_grads(worker_idx, X_local, y_local, Wid, tauid, X_shard_idx
 
 def apply_grad(W, g, eta):
     z = eta*W - g
-    z[z <= LAMBDA1] = 0
-    z[z <= -LAMBDA1] = 0
+    z[(z >= -LAMBDA1) & (z <= LAMBDA1)] = 0
     z[z > 0] -= LAMBDA1
     z[z < 0] += LAMBDA1
     z /= eta + LAMBDA2
@@ -191,7 +197,6 @@ def driver_aggregate(client, worker_idx, W, Wid, W_shard_size, n_feats,
 
     print("aggregator {} finished!".format(worker_idx))
 
-
 def fit_async(X, y):
     client = PlasmaClient(addr_info['object_store_name'])
 
@@ -216,31 +221,33 @@ def fit_async(X, y):
         W_shard_queues[i, :] = [[] for _ in range(NUM_WORKERS)]
 
     handle_matrix = np.empty((NUM_WORKERS, NUM_WORKER_ITERS), dtype=object)
-    for i in range(NUM_WORKERS):
-      dprint("about to start worker {}".format(i))
-      shard_range = (i*X_shard_size, min((i+1)*X_shard_size, n_features))
-      y_shard = y[range(*shard_range)]
-      X_shard = X[range(*shard_range)]
-      iter_handles = async_compute_lr_grads.remote(
-          i, X_shard, y_shard, Wid, tauid, i, X_shard_size, W_shard_size,
-          NUM_W_SHARDS, # TODO: not always true
-          n_features)
-      handle_matrix[i, :] = iter_handles
-
-    total_t = 0
-    eta = BETA + np.sqrt(total_t) / ALPHA
 
     def thread_driver_aggregate(i):
         driver_aggregate(client, i, W, Wid, W_shard_size, n_features,
                          handle_matrix[i], W_shard_queues, W_shard_locks,
                          eta, total_t, tau, tauid)
 
-    pool = ThreadPool(NUM_WORKERS)
-    pool.map(thread_driver_aggregate, range(NUM_WORKERS))
+    total_t = 0
+    eta = BETA + np.sqrt(total_t) / ALPHA
 
-    # def thread_driver_aggregate(worker_idx):
-    #     return driver_aggregate(worker_idx, W, Wid, handle_matrix[worker_idx, :], W_shard_queues)
-    # driver_pool.map(thread_driver_aggregate, range(NUM_WORKERS))
+    pool = ThreadPool(NUM_WORKERS)
+
+    for mi in range(MAJOR_ITERS):
+        print("mi {}, loss {}".format(mi, logistic_loss(W, X, y, ALPHA)))
+        for i in range(NUM_WORKERS):
+            dprint("about to start worker {}".format(i))
+            shard_range = (i*X_shard_size, min((i+1)*X_shard_size, n_features))
+            y_shard = y[range(*shard_range)]
+            X_shard = X[range(*shard_range)]
+            iter_handles = async_compute_lr_grads.remote(
+                i, X_shard, y_shard, Wid, tauid, i, X_shard_size, W_shard_size,
+                NUM_W_SHARDS, # TODO: not always true
+                n_features)
+            handle_matrix[i, :] = iter_handles
+
+        pool.map(thread_driver_aggregate, range(NUM_WORKERS))
+        print("finished major iter {}".format(mi+1))
+    print("mi {}, loss {}".format(mi, logistic_loss(W, X, y, ALPHA)))
 
 if __name__ == '__main__':
     def load_data():
